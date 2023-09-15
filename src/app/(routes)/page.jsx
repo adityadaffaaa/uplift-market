f852510f1a17372facbf5ce3d8553b16ad7ea20b
Daffa Fawwaz Aditya <aditydaffafawwaz@gmail.com>
2023-09-15 11:07:20 +0700

    style(home): add some sections

--- src/app/(routes)/page.jsx
+++ src/app/(routes)/page.jsx
@@ -1,12 +1,25 @@
 import React from "react";
 import HeroSection from "./components/HeroSection";
 import CategorySection from "./components/CategorySection";
-
+import FrequentlyUsedServicesSection from "./components/FrequentlyUsedServicesSection";
+import WhyChooseUsSection from "./components/WhyChooseUsSection";
+import TestimonialSection from "./components/TestimonialSection";
+import EasyStepsToStartedSection from "./components/EasyStepsToStartedSection";
+import TrustedBySection from "./components/TrustedBySection";
+import FaqSection from "./components/FaqSection";
+import GetStartedSection from "./components/GetStartedSection";
 const Home = () => {
   return (
     <>
       <HeroSection />
       <CategorySection />
+      <FrequentlyUsedServicesSection />
+      <WhyChooseUsSection />
+      <EasyStepsToStartedSection />
+      <TestimonialSection />
+      <TrustedBySection />
+      <FaqSection />
+      <GetStartedSection />
     </>
   );
 };
