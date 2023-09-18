4c95e1ce1407ce22940fad797dc7ea2b94e49b39
Daffa Fawwaz Aditya <aditydaffafawwaz@gmail.com>
2023-09-18 08:18:25 +0700

    style(loading): update loading page

--- src/app/loading.jsx
+++ src/app/loading.jsx
@@ -3,7 +3,7 @@ import React from "react";
 const Loading = () => {
   return (
     <div className="h-[100vh] grid place-items-center">
-      <span className="loading loading-dots loading-lg"></span>
+      <span className="loading loading-dots loading-lg text-primary"></span>
     </div>
   );
 };
