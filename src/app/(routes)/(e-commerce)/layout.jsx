bdec7cd4fec0b44c6c0f5dda7cd74047f82ffef1
Kev9794 <k.natanael88@gmail.com>
2023-09-25 10:46:10 +0700

    feat(search filter result): add ui drawer and product list

---
+++ src/app/(routes)/(e-commerce)/layout.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const ECommerceLayout = ({ children }) => {
+  return <div className="h-full grid place-items-center">{children}</div>;
+};
+
+export default ECommerceLayout;
