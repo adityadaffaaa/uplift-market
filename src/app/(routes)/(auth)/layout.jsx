3a53d34f04af9f3d3963f4322fbc9c26b0fb65a6
Daffa Fawwaz Aditya <adityadaffafawwaz@gmail.com>
2023-10-02 08:21:20 +0700

    style(layout): update auth layout

--- src/app/(routes)/(auth)/layout.jsx
+++ src/app/(routes)/(auth)/layout.jsx
@@ -1,6 +1,6 @@
 "use client";
 
-import React, { useEffect } from "react";
+import React from "react";
 import Image from "next/image";
 import { usePathname, redirect } from "next/navigation";
 import { Cookies } from "react-cookie";
@@ -8,20 +8,17 @@ import { Cookies } from "react-cookie";
 const AuthLayout = ({ children }) => {
   const pathName = usePathname();
   const cookies = new Cookies();
+  const token = cookies.get("token");
 
-  useEffect(() => {
-    const token = cookies.get("token");
-    if (token && token !== undefined) {
+  const HandleLayout = () => {
+    if (pathName === "/register-vendor") {
+      return <RegisterVendorLayout />;
+    } else if (token && token !== undefined) {
       redirect("/");
+    } else {
+      return <UserAuthLayout />;
     }
-  }, [cookies]);
-
-  const HandleLayout = () =>
-    pathName === "/register-vendor" ? (
-      <RegisterVendorLayout />
-    ) : (
-      <UserAuthLayout />
-    );
+  };
 
   const RegisterVendorLayout = () => (
     <div className="h-full grid place-items-center md:bg-greyBackground">
