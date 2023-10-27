aea24a40b7d4d7a2af7348c831844955976ea682
Daffa Fawwaz Aditya <adityadaffafawwaz@gmail.com>
2023-10-27 13:50:04 +0700

    style(card): update card product list component

--- src/app/(routes)/components/ProductList.jsx
+++ src/app/(routes)/components/ProductList.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import icons from "@/app/utils/icons";
+import Link from "next/link";
 
 const { LocationIcon, StarIcon } = icons.homeScreenIcon;
 export const ProductList = ({ categoryNumber }) => {
@@ -60,41 +61,43 @@ const ProductCardItem = ({
   city,
 }) => {
   return (
-    <div className="card cursor-pointer card-compact w-full bg-base-100 rounded-xl border-2 transition-default hover:shadow-defaultShadow">
-      <figure>
-        <img
-          src={imgUrl}
-          alt="Shoes"
-          loading="lazy"
-          className="w-full"
-        />
-      </figure>
-      <div className="text-start py-3 px-2 text-textBlack flex flex-col gap-4">
-        <h2 className=" text-paragraph8Res lg:text-heading6">
-          {title}
-        </h2>
-        <p className="text-primary text-paragraph7Res lg:text-paragraph6">
-          Rp{price}
-        </p>
-        <div className="flex flex-col gap-1">
-          <div className="flex gap-2 items-center">
-            <StarIcon />
-            <p className="text-paragraph8Res lg:text-paragraph9 flex gap-1">
-              {rate}
-              <a className="text-paragraph9Res text-grey2 lg:text-paragraph10">
-                ({review} reviews)
-              </a>
-            </p>
-          </div>
-          <div className="flex gap-2 text-grey2 items-center">
-            <LocationIcon />
-            <p className="text-paragraph8Res lg:text-paragraph10">
-              {city}
-            </p>
+    <Link href={"product-detail"}>
+      <div className="card cursor-pointer card-compact w-full bg-base-100 rounded-xl border-2 transition-default hover:shadow-defaultShadow">
+        <figure>
+          <img
+            src={imgUrl}
+            alt="Shoes"
+            loading="lazy"
+            className="w-full"
+          />
+        </figure>
+        <div className="text-start py-3 px-2 text-textBlack flex flex-col gap-4">
+          <h2 className=" text-paragraph8Res lg:text-heading6">
+            {title}
+          </h2>
+          <p className="text-primary text-paragraph7Res lg:text-paragraph6">
+            Rp{price}
+          </p>
+          <div className="flex flex-col gap-1">
+            <div className="flex gap-2 items-center">
+              <StarIcon />
+              <p className="text-paragraph8Res lg:text-paragraph9 flex gap-1">
+                {rate}
+                <a className="text-paragraph9Res text-grey2 lg:text-paragraph10">
+                  ({review} reviews)
+                </a>
+              </p>
+            </div>
+            <div className="flex gap-2 text-grey2 items-center">
+              <LocationIcon />
+              <p className="text-paragraph8Res lg:text-paragraph10">
+                {city}
+              </p>
+            </div>
           </div>
         </div>
       </div>
-    </div>
+    </Link>
   );
 };
 
