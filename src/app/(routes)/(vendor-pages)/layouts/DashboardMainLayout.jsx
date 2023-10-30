6655e464093b194a2f13f0188dbd4a2ad487cb4c
Daffa Fawwaz Aditya <adityadaffafawwaz@gmail.com>
2023-10-30 13:09:56 +0700

    style(vendor dashboard): add context sidebar component

---
+++ src/app/(routes)/(vendor-pages)/layouts/DashboardMainLayout.jsx
@@ -0,0 +1,44 @@
+import React from "react";
+import { Button } from "@nextui-org/react";
+import icons from "@/app/utils/icons";
+import { Tabs, Tab } from "@nextui-org/react";
+import { DashboardNavbar } from "../components";
+
+const { AddIcon } = icons.vendorDashboard.productListVendor;
+
+export const DashboardMainLayout = ({ children }) => {
+  return (
+    <>
+      <DashboardNavbar />
+      <section className="w-full lg:w-[calc(100%-280px)] p-5 lg:pt-8 lg:px-8 pb-3 flex flex-col gap-9 mt-16 lg:mt-0">
+        <div className="flex justify-between items-center ">
+          <div className="flex flex-col gap-4">
+            <p className="text-paragraph3 lg:text-heading1Res">
+              Produk Saya
+            </p>
+            <Tabs
+              variant="underlined"
+              aria-label="Tabs variants"
+              className="hidden lg:block"
+              color="primary"
+            >
+              <Tab key="all" title="Semua Produk (10)" />
+              <Tab key="active" title="Aktif" />
+              <Tab key="nonActive" title="Nonaktif" />
+            </Tabs>
+          </div>
+          <Button
+            color="primary"
+            radius="sm"
+            startContent={<AddIcon />}
+          >
+            Tambah Produk
+          </Button>
+        </div>
+        {children}
+      </section>
+    </>
+  );
+};
+
+export default DashboardMainLayout;
