ac6c834c65fdccd5f7e7cf2e463d516b09629c51
Daffa Fawwaz Aditya <adityadaffafawwaz@gmail.com>
2023-11-15 11:27:57 +0700

    style(skeleton): refactor & add product list skeleton

--- src/app/components/NextSkeletons.jsx
+++ src/app/components/NextSkeletons.jsx
@@ -1,52 +1,60 @@
 import React from "react";
 import { Skeleton } from "@nextui-org/react";
 
-export const useSkeletons = () => {
-  const GoogleMapsSkeleton = () => {
-    return (
-      <>
-        <Skeleton className="w-[20%] h-10 rounded-lg">
-          <div className="h-3 w-3/5 rounded-lg bg-default-200"></div>
+export const GoogleMapsSkeleton = () => {
+  return (
+    <>
+      <Skeleton className="w-[20%] h-10 rounded-lg">
+        <div className="h-3 w-3/5 rounded-lg bg-default-200"></div>
+      </Skeleton>
+      <Skeleton className="rounded-lg">
+        <div className="h-[300px] rounded-lg bg-default-300"></div>
+      </Skeleton>
+      <div className="space-y-3 flex flex-col items-end">
+        <Skeleton className="w-[10%] rounded-lg">
+          <div className="h-10 w-4/5 rounded-lg bg-default-200"></div>
         </Skeleton>
-        <Skeleton className="rounded-lg">
-          <div className="h-[300px] rounded-lg bg-default-300"></div>
-        </Skeleton>
-        <div className="space-y-3 flex flex-col items-end">
+        <div className="flex justify-between items-end w-full">
           <Skeleton className="w-[10%] rounded-lg">
-            <div className="h-10 w-4/5 rounded-lg bg-default-200"></div>
+            <div className="h-5 w-2/5 rounded-lg bg-default-300"></div>
           </Skeleton>
-          <div className="flex justify-between items-end w-full">
-            <Skeleton className="w-[10%] rounded-lg">
-              <div className="h-5 w-2/5 rounded-lg bg-default-300"></div>
-            </Skeleton>
-            <Skeleton className="w-[10%] rounded-lg">
-              <div className="h-10 w-2/5 rounded-lg bg-default-300"></div>
-            </Skeleton>
-          </div>
-        </div>
-      </>
-    );
-  };
-
-  const NavbarUserSkeleton = () => {
-    return (
-      <>
-        <div className="flex items-center gap-3">
-          <Skeleton className="rounded-full w-10 h-10 p-6 ">
-            <div className="rounded-full w-10 h-10 p-6 bg-default-200"></div>
-          </Skeleton>
-          <Skeleton className="w-full rounded-full">
-            <div className="w-[100px] h-6 rounded-full bg-default-300"></div>
+          <Skeleton className="w-[10%] rounded-lg">
+            <div className="h-10 w-2/5 rounded-lg bg-default-300"></div>
           </Skeleton>
         </div>
-      </>
-    );
-  };
-
-  return {
-    GoogleMapsSkeleton,
-    NavbarUserSkeleton,
-  };
+      </div>
+    </>
+  );
 };
 
-export default useSkeletons;
+export const NavbarUserSkeleton = () => {
+  return (
+    <>
+      <div className="flex items-center gap-3">
+        <Skeleton className="rounded-full w-10 h-10 p-6 ">
+          <div className="rounded-full w-10 h-10 p-6 bg-default-200"></div>
+        </Skeleton>
+        <Skeleton className="w-full rounded-full">
+          <div className="w-[100px] h-6 rounded-full bg-default-300"></div>
+        </Skeleton>
+      </div>
+    </>
+  );
+};
+
+export const ProductListSkeleton = () => {
+  return (
+    <>
+      <div className="grid grid-cols-2 w-full lg:grid-cols-4 gap-4">
+        {[1, 2, 3, 4, 5, 6, 7, 8].map((value) => (
+          <Skeleton
+            key={value}
+            className="w-full h-64 lg:h-80 rounded-xl"
+          >
+            <div className="w-full h-64 lg:h-80 rounded-xl bg-default-300"></div>
+          </Skeleton>
+        ))}
+      </div>
+    </>
+  );
+};
