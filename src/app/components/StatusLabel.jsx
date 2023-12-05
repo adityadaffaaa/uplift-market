47de587125c537d02a4d7c46b5cec5bf9ad27476
Daffa Fawwaz Aditya <adityadaffafawwaz@gmail.com>
2023-12-05 08:33:17 +0700

    fix(component): update label status

--- src/app/components/StatusLabel.jsx
+++ src/app/components/StatusLabel.jsx
@@ -1,15 +1,15 @@
 import React from "react";
 
-const StatusLabel = ({ step = 1 }) => {
+const StatusLabel = ({ status = "Pesanan baru" }) => {
   return (
     <div
       className={`px-6 py-1 text-paragraph7Res rounded-full ${
-        step === 1
+        status === "Pesanan baru"
           ? "bg-[#FFF9E7] text-secondary"
           : "bg-[#E1E9FF] text-[#5378E2]"
       } `}
     >
-      {step === 1 ? "Baru" : "Dikerjakan"}
+      {status}
     </div>
   );
 };
