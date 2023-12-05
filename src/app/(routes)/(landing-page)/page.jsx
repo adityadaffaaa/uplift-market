63029b33b3672047964ea6a6b18230ae5c21ccf7
Daffa Fawwaz Aditya <adityadaffafawwaz@gmail.com>
2023-12-05 08:34:31 +0700

    refactor(page): home page

--- src/app/(routes)/(landing-page)/page.jsx
+++ src/app/(routes)/(landing-page)/page.jsx
@@ -9,39 +9,15 @@ import {
   TrustedBySection,
   FaqSection,
   GetStartedSection,
-} from "../components";
+} from "./components";
 import ProviderPage from "./components/provider";
-import { useProduct } from "@/app/hooks/user/product";
 
-const fetchProduct = async () => {
-  let setAlerts = [];
-  const { getListProduct } = useProduct();
-  try {
-    const res = await getListProduct({ setAlerts });
-    if (res.status === 200) {
-      const data = res.data.data;
-
-      return { data, setAlerts };
-    } else {
-      console.log(res);
-      return { setAlerts };
-    }
-  } catch (error) {
-    console.log(setAlerts);
-    console.error("Something wrong", error);
-  }
-};
-
-const Home = async () => {
-  const res = await fetchProduct();
+const Home = () => {
   return (
     <ProviderPage>
       <HeroSection />
       <CategorySection />
-      <FrequentlyUsedServicesSection
-        setAlerts={res?.setAlerts}
-        products={res?.data}
-      />
+      <FrequentlyUsedServicesSection />
       <WhyChooseUsSection />
       <EasyStepsToStartedSection />
       <TestimonialSection />
